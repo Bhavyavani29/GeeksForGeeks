4ddcdc251c50b10d0c5dcc36ba741a563fee1a3e
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-13 21:01:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Ugly Number II/ugly-number-ii.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    public static int uglyNumber(int n) {
+        // code here
+        int result[] = new int[n];
+        int fact2 = 0, fact3 = 0, fact5 = 0;
+        if(n <= 0) return 0;
+        result[0] = 1;
+        for(int i = 1; i < n; i++){
+            int next2 = result[fact2] * 2;
+            int next3 = result[fact3] * 3;
+            int next5 = result[fact5] * 5;
+            int nextUgly = Math.min(next2, Math.min(next3, next5));
+            result[i] = nextUgly;
+            
+            if (nextUgly == next2) fact2++;
+            if (nextUgly == next3) fact3++;
+            if (nextUgly == next5) fact5++;
+        }
+        return result[n - 1];
+    }
+}
