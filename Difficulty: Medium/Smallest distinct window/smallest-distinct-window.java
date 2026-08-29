1bbcc40471134e8f566fb858a05925cc2f6a8bf9
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-08-29 22:21:52 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Smallest distinct window/smallest-distinct-window.java
@@ -0,0 +1,26 @@
+// User function Template for Java
+
+class Solution {
+    public int findSubString(String str) {
+        // code here
+        Map<Character,Integer> hm = new HashMap<>();
+        int small = Integer.MAX_VALUE;
+        int i = 0,j = 0, unique = 0;
+        while(j < str.length()){
+            char c = str.charAt(j);
+            hm.put(c,hm.getOrDefault(c,0)+1);
+            while(i < str.length() && hm.get(str.charAt(i)) > 1){
+                hm.put(str.charAt(i),hm.get(str.charAt(i)) - 1);
+                i++;
+            }
+            if(hm.size() > unique){
+                unique = hm.size();
+                small = j-i+1;
+            }else{
+                small = Math.min(small,j-i+1);
+            }
+            j++;
+        }
+        return small;
+    }
+}
