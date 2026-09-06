bcfaf4a1ffb0d5a6e424a9d7b31370a87b0fb1da
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-06 21:40:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Union of Two Arrays with Distinct Elements/union-of-two-arrays-with-distinct-elements.java
@@ -0,0 +1,15 @@
+class Solution {
+	public ArrayList<Integer> findUnion(int[] a, int[] b) {
+		// code here
+		HashSet<Integer> hs = new HashSet<>();
+		for (int i = 0; i < a.length; i++) {
+			hs.add(a[i]);
+		}
+		for (int j = 0; j < b.length; j++) {
+			hs.add(b[j]);
+		}
+		ArrayList<Integer> al = new ArrayList<>(hs);
+		Collections.sort(al);
+		return al;
+	}
+};
