949d86b28ec3c6bf66232ca550c2b21ea62cc481
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-20 22:08:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Third Largest/third-largest.java
@@ -0,0 +1,10 @@
+class Solution {
+	public int thirdLargest(List<Integer> arr) {
+		// code here
+		if (arr.size() < 3) {
+			return - 1;
+		}
+		Collections.sort(arr);
+		return arr.get(arr.size() - 3);
+	}
+}
