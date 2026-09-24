0c00c4855fe96d9a6979e2e4b5f70d7a70d532a5
Bhavya Vani Gorrela <bhavyavani29@gmail.com>
2026-09-24 23:05:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Repeated Character/repeated-character.java
@@ -0,0 +1,16 @@
+class Solution {
+    char firstRep(String S) {
+        HashMap<Character, Integer> hm = new HashMap<>();
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < S.length(); i++) {
+            char ch = S.charAt(i);
+            if (hm.get(ch) > 1) {
+                return ch;
+            }
+        }
+        return '#';
+    }
+}
